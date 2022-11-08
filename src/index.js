64b430c11fc27856acbed8838c3549d60200e5b1
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-08 23:53:05 +0100

    props-5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+class Age extends React.Component {
+  render() {
+    return this.props.age && <p>Your age is {this.props.age}</p>
+  }
+}
 class Welcome extends React.Component {
   render() {
     return (
       <div>
         <h1>Welcome {this.props.name || "utenteeee"}!!!</h1>
-        { this.props.age && <p>Your age is {this.props.age}</p> }
+        <Age age="13" />
       </div>
     )
   }
