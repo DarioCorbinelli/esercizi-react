0ebcef9407d579853b3fb8124791ca3806ead292
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-08 23:45:20 +0100

    props-3

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 
 class Welcome extends React.Component {
   render() {
-    return <h1>Welcome {this.props.name || "utenteeee"}!!!</h1>
+    return (
+      <div>
+        <h1>Welcome {this.props.name || "utenteeee"}!!!</h1>
+        { this.props.age && <p>Your age is {this.props.age}</p> }
+      </div>
+    )
   }
 }
 
 class App extends React.Component {
   render() {
-    return <Welcome name="Dario" />
+    return <Welcome name="Dario" age="24"/>
   }
 }
 
