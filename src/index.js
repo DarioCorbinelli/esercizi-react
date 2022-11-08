8e1fa6d83230c76ce7fe5adb29c7a3fcb3df6137
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-08 22:49:08 +0100

    components-3

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello World</h1>
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+      </div>
+    )
+  }
+}
+
+ReactDOM.createRoot(document.querySelector(".app")).render(<App />)
