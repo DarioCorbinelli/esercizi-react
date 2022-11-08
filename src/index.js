81b1e8a96ab60e3eed3cd0bedccc1abf8a70a32f
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-08 22:59:37 +0100

    components-7

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+class Message extends React.Component {
+  render() {
+    return <p>What a beautiful day!</p>
+  }
+}
 class Hello extends React.Component {
   render() {
     return (
       <div>
         <h1>Hello World</h1>
-        <p>What a beautiful day!</p>
+        <Message />
       </div>
     )
   }
