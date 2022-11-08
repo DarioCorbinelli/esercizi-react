cb370dcaef2bef34cc712ddbf16ab4457d91f264
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-08 22:54:52 +0100

    components-5

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom/client";
 
 class Hello extends React.Component {
   render() {
-    return <h1>Hello World</h1>
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>What a beautiful day!</p>
+      </div>
+    )
   }
 }
 
