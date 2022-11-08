fdc206f8375d4ca81d0bd3b204fc5226cfe3267b
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-08 23:13:04 +0100

    props-1

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Welcome {this.props.name}</h1>
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return <Welcome name="Dario" />
+  }
+}
+
+ReactDOM.createRoot(document.querySelector(".app")).render(<App />)
