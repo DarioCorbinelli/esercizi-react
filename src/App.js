436cb36e347d5cda9038aa96a6af5959b76f044f
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-14 08:44:25 +0100

    fix excercise

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Hello from "./Hello.js"
+import React from "react"
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
+import App from "./App.js"
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello World</h1>
-  }
-}
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Hello />
-      </div>
-    )
-  }
-}
 
 ReactDOM.createRoot(document.querySelector(".app")).render(<App />)
