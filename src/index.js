6953332fdcafbda9df1ff48ae3add6b647ff41c2
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-14 09:20:21 +0100

    fix organization

--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-class Age extends React.Component {
-  render() {
-    return this.props.age >= 18 && <p>Your age is {this.props.age}</p>
-  }
-}
-class Welcome extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Welcome {this.props.name || "utenteeee"}!!!</h1>
-        <Age age="19" />
-      </div>
-    )
-  }
-}
-
-class App extends React.Component {
-  render() {
-    return <Welcome name={<strong>Dario</strong>}/>
-  }
-}
+import App from "./App.js"
 
 ReactDOM.createRoot(document.querySelector(".app")).render(<App />)
