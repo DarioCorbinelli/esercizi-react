6b40fe7ccba25ee8cacd4ed7c8671f8cdd2e63b4
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-14 11:46:14 +0100

    state-4

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CounterDisplay from "./CounterDisplay.js"
 
 export default class Counter extends React.Component {
   state = {
@@ -18,7 +19,7 @@ export default class Counter extends React.Component {
   }
 
   render() {
-    return <h1>{this.state.count}</h1>
+    return <CounterDisplay count={this.state.count}/>
   }
 }
 
