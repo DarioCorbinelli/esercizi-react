23765b6525d2aac0ddf4b2b3bd4b6a5a26237718
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-14 08:48:06 +0100

    fix file organization

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello World</h1>
-  }
-}
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Hello />
-      </div>
-    )
-  }
-}
+import App from "./App.js"
 
 ReactDOM.createRoot(document.querySelector(".app")).render(<App />)
