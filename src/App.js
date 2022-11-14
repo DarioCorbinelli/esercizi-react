95b6d0446971c0ed4c0494473fcc9f3ece6c2810
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-14 09:01:22 +0100

    fix organization

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Welcome from "./Welcome.js"
+
+export default class App extends React.Component {
+  render() {
+    return <Welcome name="Dario" />
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-class Welcome extends React.Component {
-  render() {
-    return <h1>Welcome {this.props.name}</h1>
-  }
-}
-
-class App extends React.Component {
-  render() {
-    return <Welcome name="Dario" />
-  }
-}
+import App from "./App.js"
 
 ReactDOM.createRoot(document.querySelector(".app")).render(<App />)
