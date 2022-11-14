49550214dd31372f9f3c43644b01c3c8fb9d04e1
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-14 08:51:25 +0100

    fix organization

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hello from "./Hello.js"
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-class Hello extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-        <p>What a beautiful day!</p>
-      </div>
-    )
-  }
-}
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Hello />
-      </div>
-    )
-  }
-}
+import App from "./App.js"
 
 ReactDOM.createRoot(document.querySelector(".app")).render(<App />)
