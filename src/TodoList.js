99e1539fb796d7647171c589fde16a3ec34c61db
DarioCorbinelli <108214071+DarioCorbinelli@users.noreply.github.com>
2022-11-22 10:04:14 +0100

    lists-4

--- src/TodoList.js
+++ src/TodoList.js
@@ -15,17 +15,22 @@ export default class TodoList extends React.Component {
     })
   }
 
-  handleSubmit = () => {
+  handleSubmit = (e) => {
+    e.preventDefault()
+    
     this.setState(state => ({
-      items: [...state.items, <li key={state.items.length + 1}>{state.input}</li>]
+      items: [...state.items, <li key={state.items.length + 1}>{state.input}</li>],
+      input: ""
     }))
   }
 
   render() {
     return (
       <>
-        <input type="text" value={this.state.input} onChange={this.handleInput}/>
-        <button onClick={this.handleSubmit}>Add todo</button>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.input} onChange={this.handleInput}/>
+          <button type="submit">Add todo</button>
+        </form>
         <ul>
           {this.state.items}
         </ul>
