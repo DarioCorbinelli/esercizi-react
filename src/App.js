11eefa74718afcf59db5a5c6f2446f454bef21b2
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-23 09:42:06 +0100

    composition-1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
-import Login from "./Login";
-
+import Container from "./Container";
 export default class App extends React.Component {
   render() {
-    return <Login />
+    return (
+      <Container>
+        <h1>Primo child el</h1>
+        <h3>Secondo child el</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, amet ipsum! Minima, vero odio totam illo aspernatur accusamus saepe cupiditate alias, ipsum asperiores sed repellendus molestias iste soluta! Eligendi, consequatur?</p>
+      </Container>
+    )
   }
 }
