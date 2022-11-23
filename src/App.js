7d38e8bac1ac5b893e269052f0d398081322eaf9
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-23 09:52:17 +0100

    composition-2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Container from "./Container";
 export default class App extends React.Component {
   render() {
     return (
-      <Container>
-        <h1>Primo child el</h1>
+      <Container title={ <h1>Questo è il titolo passato tramite prop</h1> }>
         <h3>Secondo child el</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, amet ipsum! Minima, vero odio totam illo aspernatur accusamus saepe cupiditate alias, ipsum asperiores sed repellendus molestias iste soluta! Eligendi, consequatur?</p>
       </Container>
--- src/Container.js
+++ src/Container.js
@@ -4,6 +4,7 @@ export default class Container extends React.Component {
   render() {
     return (
       <div className="container">
+        {this.props.title}
         {this.props.children}
       </div>
     )
