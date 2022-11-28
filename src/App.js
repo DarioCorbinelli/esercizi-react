e356ef24f014f097e7a39200e1561996b1bdc7f8
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-28 14:21:02 +0100

    useEffect-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import ClickCounter from "ClickCounter";
+import Counter from "Counter";
 import React from "react";
 
-
 export default class App extends React.Component {
+  state = {
+    toggleCounter: true,
+  };
+
+  handleToggle = () => {
+    this.setState(state => {
+      return {
+        toggleCounter: !state.toggleCounter
+      }
+    })
+  }
+
   render() {
-    return <ClickCounter />
+    return (
+      <>
+        <button onClick={this.handleToggle}>toggle counter</button>
+        {this.state.toggleCounter && <Counter />}
+      </>
+    );
   }
 }
