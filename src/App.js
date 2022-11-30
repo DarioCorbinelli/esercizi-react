a1e738dd0d0d326d54a3589417258fe0df021978
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-30 18:19:59 +0100

    router-1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import FilteredList from "FilteredList";
 import React from "react";
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Welcome from "Welcome";
 
 function App() {
-  const [persons, setPersons] = useState([])
-  const [newPerson, setNewPerson] = useState({
-    name: "",
-    age: ""
-  })
-
-  function handleInput(e) {
-    setNewPerson(newPerson => ({
-      ...newPerson,
-      [e.target.name]: e.target.value
-    }))
-  }
-
-  function handleCreateNewPerson() {
-    setPersons(persons => [...persons, {name: newPerson.name, id: persons.length + 1, age: newPerson.age}])
-    setNewPerson({name: "", age: ""})
-  }
-
-  return <>
-    <input type="text" name="name" placeholder="name" value={newPerson.name} onChange={handleInput}/>
-    <input type="number" name="age" placeholder="age" value={newPerson.age} onChange={handleInput}/>
-    <button onClick={handleCreateNewPerson}>Create new adult person</button>
-    <FilteredList persons={persons}/>
-  </>
+  return (
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.js"
+import App from 'App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.querySelector(".app")).render( <App /> )
+ReactDOM
+  .createRoot(document.querySelector(".app"))
+  .render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
