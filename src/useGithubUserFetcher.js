80665dea5f3189362eb7708d2e32a48840266304
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-12-01 13:24:35 +0100

    fix

--- src/useGithubUserFetcher.js
+++ src/useGithubUserFetcher.js
@@ -1,16 +1,19 @@
 import useSWR from "swr";
 
 function fetcher(url) {
-  if (url) {return fetch(url).then(response => {
-    if (response.status !== 200) throw new Error()
-    return response.json()
-  })}
+  if (url) {
+    return fetch(url).then((response) => {
+      if (response.status !== 200) throw new Error();
+      return response.json();
+    });
+  }
 }
 
 function useGithubUserFetcher(username) {
-  const {data, error} = useSWR(`https://api.github.com/users/${username}`, fetcher )
-
-  return {data, error};
+  if (username) {
+    const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher);
+    return { data, error };
+  }
 }
 
 export default useGithubUserFetcher;
